6fa7316ef889e3a32901c96d5041c7cd8127a107
clevrmick <clevrmick@gmail.com>
2021-05-26 14:48:21 +0100

    ADD: image slider

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
-import Home from './components/Home'
+import Home from './components/Home';
 
 function App() {
   return (
